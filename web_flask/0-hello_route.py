6b791012b19068e079b7f4d1abf9973e8e824366
Osunba Adeola <osunbaadeola56@gmail.com>
2024-04-20 07:49:35 -0700

    Web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
 """
-A simple Flask web application to display basic text messages on different routes.
-The application defines two routes:
-- The root ('/') which returns "Hello HBNB!"
-- '/hbnb' which returns "HBNB"
-This module demonstrates basic routing and documentation standards for Flask applications.
+A simple Flask web application to display
+basic text messages on different routes.
+This module demonstrates basic routing and
+documentation standards for Flask applications.
 """
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
-    """
-    Responds to the root URL '/' with a greeting.
-    This function returns a simple string "Hello HBNB!" when the root URL is accessed.
-    It demonstrates how to handle root-level HTTP GET requests in a Flask application.
-    """
+    "Responds to the root URL '/' with a greeting."
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
-    Responds to the URL '/hbnb' with a specific text.
     This function returns the string "HBNB" when the '/hbnb' URL is accessed.
-    It demonstrates the use of non-root level routes in a Flask application.
     """
     return "HBNB"
 
-if __name__ == '__main__':
-    """
-    Executes the Flask application only if the script is run directly.
-    This block sets the Flask application to run on all network interfaces of the host
-    and listens on port 5000.
-    """
-    app.run(host='0.0.0.0', port=5000)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
